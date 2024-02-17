9214e4cfe5f6a6c10a143232efbd322286e80530
PranavRJoshi <pranavrjoshi1@gmail.com>
2024-02-17 16:27:09 +0545

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,4 +1,10 @@
-
 print("Hello, World!")
 print("Testing for the automated build to trigger!")
 print("Testing for the automated build to trigger in pipeline build")
+
+sum = 0
+
+for x in range(100000)
+  sum += x
+
+print("The sum of the first 100000 not including the last number is: ", x)
